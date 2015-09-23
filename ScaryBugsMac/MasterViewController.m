168d5230383fc22f8aa501fa0bc5485042ce25b9
leowll <leowll.kit@gmail.com>
2015-09-23 23:40:20 +0900

    Add the detail view

--- ScaryBugsMac/MasterViewController.m
+++ ScaryBugsMac/MasterViewController.m
@@ -10,8 +10,14 @@
 
 #import "ScaryBugDoc.h"
 #import "ScaryBugData.h"
+#import "EDStarRating.h"
 
 @interface MasterViewController ()
+@property (weak) IBOutlet NSTableView *bugsTableView;
+@property (strong) IBOutlet NSTextField *bugTitleView;
+@property (weak) IBOutlet EDStarRating *bugRating;
+@property (weak) IBOutlet NSImageView *bugImageView;
+
 
 @end
 
@@ -38,8 +44,80 @@
     return cellView;
 }
 
+-(void)loadView
+{
+    [super loadView];
+    self.bugRating.starImage = [NSImage imageNamed:@"star.png"];
+    self.bugRating.starHighlightedImage = [NSImage imageNamed:@"shockedface2_full.png"];
+    self.bugRating.starImage = [NSImage imageNamed:@"shockedface2_empty.png"];
+    self.bugRating.maxRating = 5.0;
+    self.bugRating.delegate = (id<EDStarRatingProtocol>) self;
+    self.bugRating.horizontalMargin = 12;
+    self.bugRating.editable=YES;
+    self.bugRating.displayMode=EDStarRatingDisplayFull;
+    
+    
+    self.bugRating.rating= 0.0;
+}
+
+-(ScaryBugDoc*)selectedBugDoc
+{
+    NSInteger selectedRow = [self.bugsTableView selectedRow];
+    if( selectedRow >=0 && self.bugs.count > selectedRow )
+    {
+        ScaryBugDoc *selectedBug = [self.bugs objectAtIndex:selectedRow];
+        return selectedBug;
+    }
+    return nil;
+    
+}
+
+-(void)setDetailInfo:(ScaryBugDoc*)doc
+{
+    NSString    *title = @"";
+    NSImage     *image = nil;
+    float rating=0.0;
+    if( doc != nil )
+    {
+        title = doc.data.title;
+        image = doc.fullImage;
+        rating = doc.data.rating;
+    }
+    [self.bugTitleView setStringValue:title];
+    [self.bugImageView setImage:image];
+    [self.bugRating setRating:rating];
+    
+}
+
+- (void)tableViewSelectionDidChange:(NSNotification *)aNotification
+{
+    ScaryBugDoc *selectedDoc = [self selectedBugDoc];
+    
+    // Update info
+    [self setDetailInfo:selectedDoc];
+}
 
 - (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
     return [self.bugs count];
 }
+
+- (IBAction)addBug:(id)sender {
+    // 1. Create a new ScaryBugDoc object with a default name
+    ScaryBugDoc *newDoc = [[ScaryBugDoc alloc] initWithTitle:@"New Bug" rating:0.0 thumbImage:nil fullImage:nil];
+    
+    // 2. Add the new bug object to our model (insert into the array)
+    [self.bugs addObject:newDoc];
+    NSInteger newRowIndex = self.bugs.count-1;
+    
+    // 3. Insert new row in the table view
+    [self.bugsTableView insertRowsAtIndexes:[NSIndexSet indexSetWithIndex:newRowIndex] withAnimation:NSTableViewAnimationEffectGap];
+    
+    // 4. Select the new bug and scroll to make sure it's visible
+    [self.bugsTableView selectRowIndexes:[NSIndexSet indexSetWithIndex:newRowIndex] byExtendingSelection:NO];
+    [self.bugsTableView scrollRowToVisible:newRowIndex];
+}
+- (IBAction)deleteBug:(id)sender {
+}
+
+
 @end
